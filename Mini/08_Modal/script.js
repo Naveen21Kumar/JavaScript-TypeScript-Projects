6a39cc8581e09b7f3b6bca95525708db1b20747e
Naveen Kumar <kumar20maverick@gmail.com>
2026-07-14 22:22:28 +0530

    Project 08. Modal: completed Modal project with open,close, overlay, ESC key functionality

---
+++ Mini/08_Modal/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const buttons = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', openModal);
+});
+
+document.querySelector('.close-modal').addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
+});
